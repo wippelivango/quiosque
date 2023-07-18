0b58bbe4bc3c132534ff2b2eaedb0816f9481d1e
wippelivango <131816524+wippelivango@users.noreply.github.com>
2023-07-18 14:04:05 -0300

    Ajustes finais - Update QuiosqueRepository.java

--- src/main/java/com/ivango/quiosque/repository/QuiosqueRepository.java
+++ src/main/java/com/ivango/quiosque/repository/QuiosqueRepository.java
@@ -3,5 +3,8 @@ package com.ivango.quiosque.repository;
 import com.ivango.quiosque.entity.Quiosque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuiosqueRepository extends JpaRepository<Quiosque, Long> {
+  List<Quiosque> findByNomeLike(String nome);
 }
